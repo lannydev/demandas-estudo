42c3c6678b9e04b80bfd36e6457f5d73a3a07c30
Elaine <e.cristina00@gmail.com>
2025-04-01 22:10:03 -0300

    Criando o projeto lc-personalizados

---
+++ lc-personalizados/src/main/java/com/lc_personalizados/demo/LcPersonalizadosApplication.java
@@ -0,0 +1,13 @@
+package com.lc_personalizados.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcPersonalizadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcPersonalizadosApplication.class, args);
+	}
+
+}
---
+++ lc-personalizados/src/test/java/com/lc_personalizados/demo/LcPersonalizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc_personalizados.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcPersonalizadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
