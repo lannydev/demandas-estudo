9fc7f4bd5be4c7a100ce0b9c3193b7792ed45014
Elaine <e.cristina00@gmail.com>
2025-04-06 11:17:46 -0300

    Configuração do banco de dados e domain

--- lc-personalizados/src/main/java/domain/Cliente.java
+++ lc-personalizados/src/main/java/domain/Cliente.java
@@ -2,20 +2,28 @@ package domain;
 
 import dto.ClienteRequestDTO;
 import dto.ClienteResponseDTO;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "co_id")
     private Long id;
+
+    @Column(name = "co_nome")
     private String nome;
+
+    @Column(name = "co_cpf")
     private String cpf;
+
+    @Column(name = "co_telefone")
     private String telefone;
 
 
